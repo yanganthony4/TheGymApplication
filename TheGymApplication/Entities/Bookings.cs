6059ca10a30a14f293d9cc28127dfdbcdeda4f4e
kevin-liuu <119897485+kevin-liuu@users.noreply.github.com>
2023-04-19 22:14:38 -0600

    feat: added bookings class, along with populating list method

---
+++ TheGymApplication/Entities/Bookings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace TheGymApplication.Entities
+{
+    internal class Bookings : SingeltonConnection
+    {
+        public int bookingId { get; set; }
+        public int trainerId { get; set; }
+        public string bookingDate { get; set; }
+        public int bookingLength { get; set; }
+
+
+        public Bookings(int bookingId, int trainerId, string bookingDate, int bookingLength)
+        {
+            this.bookingId = bookingId;
+            this.trainerId = trainerId;
+            this.bookingDate = bookingDate;
+            this.bookingLength = bookingLength;
+        }
+
+        public static List<Bookings> PopulateBookingData()
+        {
+            List<Bookings> listbooking = new List<Bookings>();
+            using (MySqlConnection connection = Connection)
+            {
+                MySqlCommand command = new MySqlCommand("SELECT * FROM bookings", connection);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int bookingId = reader.GetInt32(0);
+                        int trainerId = reader.GetInt32(1);
+                        string bookingDate = reader.GetString(2);
+                        int bookingLength = reader.GetInt32(3);
+                        Bookings booking = new Bookings(bookingId, trainerId, bookingDate, bookingLength);
+                        listbooking.Add(booking);
+                    }
+                    return listbooking;
+                }
+            }
+        }
+
+
+    }
+}
