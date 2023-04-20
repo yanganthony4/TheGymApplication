57c3d7b689402c18a83cf71ccfa28427df65fb49
yanganthony4 <yanganthony4@gmail.com>
2023-04-19 19:50:58 -0600

    fet: Connected database and tested data retrieval methods

--- TheGymApplication/MainPage.xaml.cs
+++ TheGymApplication/MainPage.xaml.cs
@@ -1,22 +1,37 @@
 ﻿using MySqlConnector;
+using TheGymApplication;
+
 namespace TheGymApplication;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
-    
-    public MainPage()
+	public MainPage()
 	{
 		InitializeComponent();
-        this.BindingContext = new VisitCountViewModel();
+        this.BindingContext = new BindingLabel();
     }
-    public class VisitCountViewModel
+    public class BindingLabel
     {
-    
-    public int VisitCount { get; set; } = 42; // Example value
-    }
+        public string FirstName { get; set; }
+        public int VisitCount { get; set; } = 42; // Example value
 
-    
+        public BindingLabel() 
+        {
+            using(MySqlConnection connection = SingeltonConnection.Connection) // use the SingletonConnection class to get the MySqlConnection
+                {
+                MySqlCommand command = new MySqlCommand("SELECT FirstName FROM Trainers", connection);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        FirstName = reader.GetString(0); // assign the first name to the FirstName property
+                    }
+                }
+            }
+        }
+    }
 }
 
--- TheGymApplication/Trainers.cs
+++ TheGymApplication/Trainers.cs
@@ -10,28 +10,25 @@ namespace TheGymApplication
     
     public class Trainers: SingeltonConnection
     {
-        public int Trainerid { get; set; }
-        public string Fname { get; set; }
-        public string Lname { get; set; }   
-        public string Speciality { get; set; }  
-
-
-        public void PopulateTrainerName()
+        public static string GetFirstName()
         {
-            string sql = "SELECT TrainerId, FirstName, LastName, Speciality FROM Trainers";
-            MySqlCommand command = new MySqlCommand(); 
+            string firstName = null;
 
-            MySqlDataReader reader = command.ExecuteReader();
-            
-            while (reader.Read())
+            using (MySqlConnection connection = SingeltonConnection.Connection)
             {
-                this.Trainerid = reader.GetInt32(0);
-                this.Fname = reader.GetString(1);
-                this.Lname = reader.GetString(2);
-                this.Speciality = reader.GetString(3); 
-            }
-            
+                MySqlCommand command = new MySqlCommand("SELECT FirstName FROM Trainers", connection);
 
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        firstName = reader.GetString(0);
+                    }
+                }
+            }
+
+            return firstName;
         }
+      
     }
 }
